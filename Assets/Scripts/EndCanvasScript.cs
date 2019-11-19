b4a02940c94e20ef33b21be16238f9f0894c8964
sefanursoylu <48895440+sefanursoylu@users.noreply.github.com>
2019-11-19 14:29:30 +1100

    BIG COMMIT! Whole bunch of shit added for gold, new meshes, changed ui and fonts, new prefabs, lighting in mainmenu, and particles. SEFA

---
+++ Assets/Scripts/EndCanvasScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCanvasScript : MonoBehaviour
+{
+    public void GoToCharSelect()
+    {
+        SceneManager.LoadScene(1); 
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
